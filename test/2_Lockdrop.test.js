4ab784ed7d175222431d1f15337dd4b26baac895
Alexander Krupenkin <mail@akru.me>
2019-11-09 13:52:51 +0300

    Getting secp256k1 public key for substrate genesis * Recover secp256k1 public from owner transaction * Added lockdrop contract tests

--- test/2_Lockdrop.test.js
+++ test/2_Lockdrop.test.js
@@ -9,27 +9,28 @@ chai.should();
 contract('Lock', (accounts) => {
 
     describe('Lockrop locked events', () => {
-        let lock_account = accounts[0];
-        let lock_balance = web3.utils.toWei('1', 'ether')
-        let lock_time = '0';
-
-        it('Get past events', async () => {
+        it('Locking funds and emit event', async () => {
             const lockdrop = await Lockdrop.deployed();
-            await lockdrop.lock(0, {from: lock_account}).should.be.fulfilled;
-            await lib.getLocks(lockdrop.address, 0, 'latest')(web3).then(console.log);
+            let tx = await lockdrop.lock(0, {from: accounts[0], value: '1000'}).should.be.fulfilled;
+            chai.expect(tx.logs[0].event).equal('Locked');
+            chai.expect(tx.logs[0].args.eth).to.eq.BN('1000');
+            chai.expect(tx.logs[0].args.owner).equal(accounts[0]);
+
+            tx = await lockdrop.lock(0, {from: accounts[0], value: '500'}).should.be.fulfilled;
+            chai.expect(tx.logs[0].event).equal('Locked');
+            chai.expect(tx.logs[0].args.eth).to.eq.BN('500');
+            chai.expect(tx.logs[0].args.owner).equal(accounts[0]);
+
+            tx = await lockdrop.lock(0, {from: accounts[1], value: '100'}).should.be.fulfilled;
+            chai.expect(tx.logs[0].event).equal('Locked');
+            chai.expect(tx.logs[0].args.eth).to.eq.BN('100');
+            chai.expect(tx.logs[0].args.owner).equal(accounts[1]);
         });
 
-        /*
-        it('Locking funds on contract creation', async () => {
-            lock = await Lock.new(lock_account, lock_time, {value: lock_balance});
-            chai.expect(await web3.eth.getBalance(lock.address)).to.eq.BN(lock_balance);
+        it('Reject transactions without funds', async () => {
+            const lockdrop = await Lockdrop.deployed();
+            await lockdrop.lock(0).should.be.rejected;
         });
-
-        it('Unlocking funds when time reached', async () => {
-            await lock.sendTransaction({from: lock_account}).should.be.fulfilled;
-            chai.expect(await web3.eth.getBalance(lock.address)).to.eq.BN('0');
-        });
-        */
     });
 
 });
