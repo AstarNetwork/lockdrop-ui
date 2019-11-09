5bdc25d4c5ec52a955847a86a0fca7c3c73dc000
Alexander Krupenkin <mail@akru.me>
2019-11-09 14:31:48 +0300

    Updated according to latest specs

--- contracts/Lockdrop.sol
+++ contracts/Lockdrop.sol
@@ -3,19 +3,13 @@ pragma solidity ^0.5.0;
 import './Lock.sol';
 
 contract Lockdrop {
-    enum Term {
-        ThreeMo,
-        SixMo,
-        TwelveMo
-    }
-
     // Time constants
     uint256 constant public LOCK_DROP_PERIOD = 1 days * 92; // 3 months
     uint256 public LOCK_START_TIME;
     uint256 public LOCK_END_TIME;
 
     // ETH locking events
-    event Locked(address indexed owner, uint256 indexed eth, address indexed lockAddr);
+    event Locked(uint256 indexed eth, uint256 indexed duration, address lock);
 
     constructor(uint startTime) public {
         LOCK_START_TIME = startTime;
@@ -24,26 +18,32 @@ contract Lockdrop {
 
     /**
      * @dev        Locks up the value sent to contract in a new Lock
-     * @param      term         The length of the lock up\
+     * @param      _days         The length of the lock up
      */
-    function lock(Term term)
+    function lock(uint256 _days)
         external
         payable
         didStart
         didNotEnd
     {
-        require(msg.value > 0);
+        // Accept External Owned Accounts only
+        require(msg.sender == tx.origin);
 
+        // Accept only fixed set of durations
+        require(_days == 30 || _days == 100 || _days == 300 || _days == 1000); 
+        uint256 unlockTime = now + _days * 1 days;
+
+        // Accept non-zero payments only
+        require(msg.value > 0);
         uint256 eth = msg.value;
-        address owner = msg.sender;
-        uint256 unlockTime = unlockTimeForTerm(term);
 
         // Create ETH lock contract
-        Lock lockAddr = (new Lock).value(eth)(owner, unlockTime);
-        // ensure lock contract has all ETH, or fail
-        assert(address(lockAddr).balance == msg.value);
+        Lock lockAddr = (new Lock).value(eth)(msg.sender, unlockTime);
 
-        emit Locked(owner, eth, address(lockAddr));
+        // ensure lock contract has all ETH, or fail
+        assert(address(lockAddr).balance == eth);
+
+        emit Locked(eth, _days, address(lockAddr));
     }
 
     /**
@@ -60,13 +60,5 @@ contract Lockdrop {
     modifier didNotEnd() {
         require(now <= LOCK_END_TIME);
         _;
-    }
-
-    function unlockTimeForTerm(Term term) internal view returns (uint256) {
-        if (term == Term.ThreeMo) return now + 92 days;
-        if (term == Term.SixMo) return now + 183 days;
-        if (term == Term.TwelveMo) return now + 365 days;
-
-        revert();
     }
 }
--- test/2_Lockdrop.test.js
+++ test/2_Lockdrop.test.js
@@ -11,23 +11,25 @@ contract('Lock', (accounts) => {
     describe('Lockrop locked events', () => {
         it('Locking funds and emit event', async () => {
             const lockdrop = await Lockdrop.deployed();
-            let tx = await lockdrop.lock(0, {from: accounts[0], value: '1000'}).should.be.fulfilled;
+            let tx = await lockdrop.lock(30, {from: accounts[0], value: '1000'}).should.be.fulfilled;
             chai.expect(tx.logs[0].event).equal('Locked');
             chai.expect(tx.logs[0].args.eth).to.eq.BN('1000');
-            chai.expect(tx.logs[0].args.owner).equal(accounts[0]);
 
-            tx = await lockdrop.lock(0, {from: accounts[0], value: '500'}).should.be.fulfilled;
+            tx = await lockdrop.lock(30, {from: accounts[0], value: '500'}).should.be.fulfilled;
             chai.expect(tx.logs[0].event).equal('Locked');
             chai.expect(tx.logs[0].args.eth).to.eq.BN('500');
-            chai.expect(tx.logs[0].args.owner).equal(accounts[0]);
 
-            tx = await lockdrop.lock(0, {from: accounts[1], value: '100'}).should.be.fulfilled;
+            tx = await lockdrop.lock(30, {from: accounts[1], value: '100'}).should.be.fulfilled;
             chai.expect(tx.logs[0].event).equal('Locked');
             chai.expect(tx.logs[0].args.eth).to.eq.BN('100');
-            chai.expect(tx.logs[0].args.owner).equal(accounts[1]);
         });
 
-        it('Reject transactions without funds', async () => {
+        it('Reject transaction without funds', async () => {
+            const lockdrop = await Lockdrop.deployed();
+            await lockdrop.lock(30).should.be.rejected;
+        });
+
+        it('Reject transaction with wrong duration', async () => {
             const lockdrop = await Lockdrop.deployed();
             await lockdrop.lock(0).should.be.rejected;
         });
