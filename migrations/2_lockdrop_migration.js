7a54e1a9ce09dbd37f036a893e5221b6975df256
Alexander Krupenkin <mail@akru.me>
2020-03-12 10:35:57 +0300

    Added mainnet migration support

--- migrations/2_lockdrop_migration.js
+++ migrations/2_lockdrop_migration.js
@@ -1,6 +1,11 @@
 const Lockdrop = artifacts.require('Lockdrop');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
   const currentTime = Math.floor(Date.now() / 1000);
-  deployer.deploy(Lockdrop, currentTime);
+  if (network.startsWith('mainnet')) { 
+      const lockdropStart = '1584230400'; // 15th March
+      deployer.deploy(Lockdrop, lockdropStart);
+  } else {
+    deployer.deploy(Lockdrop, currentTime);
+  }
 };
--- truffle.js
+++ truffle.js
@@ -75,6 +75,22 @@ module.exports = {
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
+    mainnet: {
+      provider: () => {
+        const mnemonic = fs.readFileSync(path.join(__dirname, '.secret')).toString().trim();
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/'+infuraKey)
+      },
+      network_id: 1,       // Ethereum mainnet id
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    // Useful for private networks
+    // private: {
+      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+      // network_id: 2111,   // This network is yours, in the cloud.
+
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
