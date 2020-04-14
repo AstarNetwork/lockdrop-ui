f46089c15f37e7bb3be0abca3b7bf98cbaec7afe
Hoon Kim <hoonsubin@icloud.com>
2020-04-13 21:28:51 -0600

    fixed total lock display error

--- app/src/components/LockdropCountdownPanel.tsx
+++ app/src/components/LockdropCountdownPanel.tsx
@@ -6,9 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { TimeFormat } from '../models/LockdropModels';
 import moment, { Moment, duration } from 'moment';
-import BigNumber from 'bignumber.js';
-import Web3Utils from 'web3-utils';
-import { LockTxArray } from '../models/LockdropModels';
+import { getTotalLockVal, getAllLockEvents } from '../helpers/lockdrop/EthereumLockdrop';
 
 interface Props {
     startTime: Moment;
@@ -68,32 +66,35 @@ const LockdropCountdownPanel: React.FC<Props> = ({ startTime, endTime }) => {
 
     const [timeLeft, setTimeLeft] = useState<TimeFormat>(calculateTimeLeft());
     const [lockState, setLockState] = useState(getLockState());
-    const [totalLockVal, setTotalLockVal] = useState<number>(0);
+    const [totalLockVal, setTotalLockVal] = useState('0');
 
     const getLockValue = async (): Promise<void> => {
-        const url =
-            'https://api.etherscan.io/api?module=account&action=txlist&address=0x458dabf1eff8fcdfbf0896a6bd1f457c01e2ffd6&startblock=0&endblock=latest&sort=asc';
-
         try {
-            const res = await fetch(url);
-            const data = await res.json();
-            const result: LockTxArray = data.result;
-            let totalVal = new BigNumber(0);
+            const allLocks = await getAllLockEvents(window.web3, window.contract);
+            const totalLockVal = await getTotalLockVal(allLocks, window.web3);
+            setTotalLockVal(totalLockVal);
+            // const url =
+            //     'https://api.etherscan.io/api?module=account&action=txlist&address=0x458dabf1eff8fcdfbf0896a6bd1f457c01e2ffd6&startblock=0&endblock=latest&sort=asc';
 
-            // Memo: forEach will occur `forEach Is Not a Function` error sometime
-            for (let i = 0; i < result.length; i++) {
-                const txVal = new BigNumber(result[i].value);
-                totalVal = totalVal.plus(txVal);
-            }
+            // const res = await fetch(url);
+            // const data = await res.json();
+            // const result: LockTxArray = data.result;
+            // let totalVal = new BigNumber(0);
 
-            // Memo: Recursion
-            if (totalVal.s !== null) {
-                setTotalLockVal(
-                    Number(new BigNumber(Web3Utils.fromWei(totalVal.toFixed(), 'ether')).decimalPlaces(1).toFixed()),
-                );
-            } else {
-                getLockValue();
-            }
+            // // Memo: forEach will occur `forEach Is Not a Function` error sometime
+            // for (let i = 0; i < result.length; i++) {
+            //     const txVal = new BigNumber(result[i].value);
+            //     totalVal = totalVal.plus(txVal);
+            // }
+
+            // // Memo: Recursion
+            // if (totalVal.s !== null) {
+            //     setTotalLockVal(
+            //         Number(new BigNumber(Web3Utils.fromWei(totalVal.toFixed(), 'ether')).decimalPlaces(1).toFixed()),
+            //     );
+            // } else {
+            //     getLockValue();
+            // }
         } catch (err) {
             console.error(err);
         }
