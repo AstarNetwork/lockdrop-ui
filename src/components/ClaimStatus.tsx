b6dbe7b0f392af0da2af49b05293d717eb656bc0
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2020-07-28 16:37:18 +0900

    optimized lock claim status panel

--- src/components/ClaimStatus.tsx
+++ src/components/ClaimStatus.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { ApiPromise } from '@polkadot/api';
 import * as plasmUtils from '../helpers/plasmUtils';
 import * as btcLockdrop from '../helpers/lockdrop/BitcoinLockdrop';
@@ -99,8 +99,6 @@ const useStyles = makeStyles(theme =>
     }),
 );
 
-// helper functions
-
 const truncateString = (str: string, num: number) => {
     if (str.length <= num) {
         return str;
@@ -116,16 +114,42 @@ const epochToDays = (epoch: number) => {
 
 const ClaimStatus: React.FC<Props> = ({ claimParams, plasmApi, plasmNetwork = 'Plasm', networkType, publicKey }) => {
     const classes = useStyles();
+    const plasmAddr = plasmUtils.generatePlmAddress(publicKey);
     const [positiveVotes, setPositiveVotes] = useState(0);
     const [voteThreshold, setVoteThreshold] = useState(0);
-    const [isLoading, setLoading] = useState(true);
-    const [plasmAddr, setPlasmAddr] = useState('');
+    const [isLoadingBal, setLoadingBal] = useState(true);
+    const [isLoadingClaims, setLoadingClaims] = useState(true);
     const [balance, setBalance] = useState('');
+    const [claims, setClaims] = useState<(Claim | undefined)[]>([]);
 
-    // set plasm address
+    const fetchLockData = useCallback(async () => {
+        // create claims IDs from all the lock parameters
+        const claimIds = claimParams.map(c => {
+            // get claim ID of current parameter
+            return plasmUtils.createLockParam(
+                c.type,
+                c.transactionHash.toHex(),
+                c.publicKey.toHex(),
+                c.duration.toString(),
+                c.value.toString(),
+            ).hash;
+        });
+
+        const lockdropStates = claimIds.map(async id => {
+            // parse plasm node to check claim status
+            const claimRes = await plasmUtils.getClaimStatus(plasmApi, id);
+            return claimRes;
+        });
+
+        setClaims(await Promise.all(lockdropStates));
+    }, [claimParams, plasmApi]);
+
+    // initial set claim status
     useEffect(() => {
-        setPlasmAddr(plasmUtils.generatePlmAddress(publicKey));
-    }, [publicKey]);
+        fetchLockData().finally(() => {
+            setLoadingClaims(false);
+        });
+    }, [fetchLockData]);
 
     // fetch address balance periodically
     useEffect(() => {
@@ -136,8 +160,10 @@ const ClaimStatus: React.FC<Props> = ({ claimParams, plasmApi, plasmNetwork = 'P
             setBalance(formatBal);
             setPositiveVotes(_voteReq.positiveVotes);
             setVoteThreshold(_voteReq.voteThreshold);
-            isLoading && setLoading(false);
-        }, 3000);
+            isLoadingBal && setLoadingBal(false);
+
+            await fetchLockData();
+        }, 5000);
 
         // cleanup hook
         return () => {
@@ -151,7 +177,7 @@ const ClaimStatus: React.FC<Props> = ({ claimParams, plasmApi, plasmNetwork = 'P
                 Sending to {plasmAddr}
             </Typography>
 
-            {plasmAddr && balance && (
+            {balance && (
                 <Typography variant="body1" component="p" align="center">
                     Has balance of {balance + ' '}
                     {plasmNetwork === 'Plasm' ? 'PLM' : 'PLD'}
@@ -161,7 +187,7 @@ const ClaimStatus: React.FC<Props> = ({ claimParams, plasmApi, plasmNetwork = 'P
             <List className={classes.listRoot} subheader={<li />}>
                 <li className={classes.listSection}>
                     <ul className={classes.ul}>
-                        {isLoading ? (
+                        {isLoadingBal || isLoadingClaims ? (
                             <div className={classes.emptyPanel}>
                                 <CircularProgress />
                             </div>
@@ -170,7 +196,7 @@ const ClaimStatus: React.FC<Props> = ({ claimParams, plasmApi, plasmNetwork = 'P
                                 <ListSubheader>You can claim {claimParams.length} locks</ListSubheader>
                                 <Divider />
 
-                                {claimParams.map(e => (
+                                {claimParams.map((e, i) => (
                                     <div key={e.transactionHash.toHex()}>
                                         <ClaimItem
                                             lockParam={e}
@@ -179,6 +205,7 @@ const ClaimStatus: React.FC<Props> = ({ claimParams, plasmApi, plasmNetwork = 'P
                                             networkType={networkType}
                                             positiveVotes={positiveVotes}
                                             voteThreshold={voteThreshold}
+                                            claimData={claims[i]}
                                         />
                                     </div>
                                 ))}
@@ -209,6 +236,7 @@ interface ItemProps {
     networkType: 'BTC' | 'ETH';
     positiveVotes: number;
     voteThreshold: number;
+    claimData?: Claim;
 }
 
 const ClaimItem: React.FC<ItemProps> = ({
@@ -218,9 +246,9 @@ const ClaimItem: React.FC<ItemProps> = ({
     networkType,
     positiveVotes,
     voteThreshold,
+    claimData,
 }) => {
     const classes = useStyles();
-    const [claimData, setClaimData] = useState<Claim>();
     const [claimId, setClaimId] = useState<H256>(
         plasmUtils.createLockParam(
             lockParam.type,
@@ -298,7 +326,6 @@ const ClaimItem: React.FC<ItemProps> = ({
                         lockParam.value.toString(),
                     ).hash,
                 );
-                setClaimData(i);
                 // turn off loading if it's on
                 if (i) {
                     setVoteList(i);
@@ -320,7 +347,6 @@ const ClaimItem: React.FC<ItemProps> = ({
         const interval = setInterval(async () => {
             const _claim = await plasmUtils.getClaimStatus(plasmApi, claimId);
             if (_claim) {
-                setClaimData(_claim);
                 setVoteList(_claim);
                 // turn off loading if it's on
                 if (sendingRequest) setSendingRequest(false);
