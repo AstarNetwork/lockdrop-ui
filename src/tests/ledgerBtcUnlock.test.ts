a8d6d5a4f9d57d3a292211a6b4d4b81961a7704d
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2020-08-05 19:29:46 +0900

    fix linting errors

--- src/tests/ledgerBtcUnlock.test.ts
+++ src/tests/ledgerBtcUnlock.test.ts
@@ -3,16 +3,15 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import AppBtc from '@ledgerhq/hw-app-btc';
-import * as bitcoinjs from 'bitcoinjs-lib';
-import Transport from '@ledgerhq/hw-transport';
+// import * as bitcoinjs from 'bitcoinjs-lib';
 // this JS library is only used for testing, so we ignore types
 // @ts-ignore
 import { createTransportReplayer, RecordStore } from '@ledgerhq/hw-transport-mocker';
 
 // lock transaction made with BTC test net from Ledger.
 // this transaction was sent from a SegWit wallet
-import lockTx from './data/ledgerBtcLock.json';
-const ADDRESS_PATH = "m/44'/1'/0'";
+// import lockTx from './data/ledgerBtcLock.json';
+// const ADDRESS_PATH = "m/44'/1'/0'";
 
 jest.setTimeout(60 * 1000);
 describe('Test HID connection with Ledger from node', () => {
@@ -23,7 +22,7 @@ describe('Test HID connection with Ledger from node', () => {
             <= 410486b865b52b753d0a84d09bc20063fab5d8453ec33c215d4019a5801c9c6438b917770b2782e29a9ecc6edb67cd1f0fbf05ec4c1236884b6d686d6be3b1588abb2231334b453654666641724c683466564d36756f517a7673597135767765744a63564dbce80dd580792cd18af542790e56aa813178dc28644bb5f03dbd44c85f2d2e7a9000
             `),
         );
-        const transport: Transport<string> = await Transport.open();
+        const transport = await Transport.open();
         const btc = new AppBtc(transport);
         const result = await btc.getWalletPublicKey("44'/0'/0'/0");
         expect(result).toEqual({
@@ -71,7 +70,7 @@ describe('Test HID connection with Ledger from node', () => {
             <= 3145022100ff492ad0b3a634aa7751761f7e063bf6ef4148cd44ef8930164580d5ba93a17802206fac94b32e296549e2e478ce806b58d61cfacbfed35ac4ceca26ac531f92b20a019000    
             `),
         );
-        const transport: Transport<string> = await Transport.open();
+        const transport = await Transport.open();
         const btc = new AppBtc(transport);
         const tx1 = btc.splitTransaction(
             '01000000014ea60aeac5252c14291d428915bd7ccd1bfc4af009f4d4dc57ae597ed0420b71010000008a47304402201f36a12c240dbf9e566bc04321050b1984cd6eaf6caee8f02bb0bfec08e3354b022012ee2aeadcbbfd1e92959f57c15c1c6debb757b798451b104665aa3010569b49014104090b15bde569386734abf2a2b99f9ca6a50656627e77de663ca7325702769986cf26cc9dd7fdea0af432c8e2becc867c932e1b9dd742f2a108997c2252e2bdebffffffff0281b72e00000000001976a91472a5d75c8d2d0565b656a5232703b167d50d5a2b88aca0860100000000001976a9144533f5fb9b4817f713c48f0bfe96b9f50c476c9b88ac00000000',
@@ -124,7 +123,7 @@ describe('Test HID connection with Ledger from node', () => {
             <= 3045022100b5b1813992282b9a1fdd957b9751d79dc21018abc6586336e272212cc89cfe84022053765a1da0bdb5a0631a9866f1fd4c583589d5188b11cfa302fc20cd2611a71e019000    
             `),
         );
-        const transport: Transport<string> = await Transport.open();
+        const transport = await Transport.open();
         const btc = new AppBtc(transport);
         const tx1 = btc.splitTransaction(
             '01000000014ea60aeac5252c14291d428915bd7ccd1bfc4af009f4d4dc57ae597ed0420b71010000008a47304402201f36a12c240dbf9e566bc04321050b1984cd6eaf6caee8f02bb0bfec08e3354b022012ee2aeadcbbfd1e92959f57c15c1c6debb757b798451b104665aa3010569b49014104090b15bde569386734abf2a2b99f9ca6a50656627e77de663ca7325702769986cf26cc9dd7fdea0af432c8e2becc867c932e1b9dd742f2a108997c2252e2bdebffffffff0281b72e00000000001976a91472a5d75c8d2d0565b656a5232703b167d50d5a2b88aca0860100000000001976a9144533f5fb9b4817f713c48f0bfe96b9f50c476c9b88ac00000000',
@@ -156,7 +155,7 @@ describe('Test HID connection with Ledger from node', () => {
             <= 3045022100e32b32b8a6b4228155ba4d1a536d8fed9900606663fbbf4ea420ed8e944f9c18022053c97c74d2f6d8620d060584dc7886f5f3003684bb249508eb7066215172281a9000
             `),
         );
-        const transport: Transport<string> = await Transport.open();
+        const transport = await Transport.open();
 
         const btc = new AppBtc(transport);
         const result = await btc.signMessageNew("44'/0'/0'/0", Buffer.from('test').toString('hex'));
@@ -259,7 +258,7 @@ describe('Test HID connection with Ledger from node', () => {
             <= 3045022100932934ee326c19c81b72fb03cec0fb79ff980a8076639f77c7edec35bd59da1e02205e4030e8e0fd2405f6db2fe044c49d3f191adbdc0e05ec7ed4dcc4c6fe7310e5019000    
             `),
         );
-        const transport: Transport<string> = await Transport.open();
+        const transport = await Transport.open();
 
         const btc = new AppBtc(transport);
 
@@ -297,7 +296,7 @@ describe('Test HID connection with Ledger from node', () => {
             `),
         );
 
-        const transport: Transport<string> = await Transport.open();
+        const transport = await Transport.open();
 
         const btc = new AppBtc(transport);
 
