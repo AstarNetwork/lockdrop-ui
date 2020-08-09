d0202efbbb1c5dd34dfc8445b76cb0f4363abb2a
Hoon Kim <hoonsubin@icloud.com>
2020-08-09 06:09:43 -0700

    removed unused buttons

--- src/pages/DustyBtcLockPage.tsx
+++ src/pages/DustyBtcLockPage.tsx
@@ -18,14 +18,14 @@ import Footer from '../components/Footer';
 import SectionCard from '../components/SectionCard';
 import { Typography, Link, makeStyles, createStyles } from '@material-ui/core';
 import quantstampLogo from '../resources/quantstamp-logo.png';
-import trezorLogo from '../resources/trezor_logo.svg';
-import ledgerLogo from '../resources/ledger_logo.svg';
+//import trezorLogo from '../resources/trezor_logo.svg';
+//import ledgerLogo from '../resources/ledger_logo.svg';
 //import * as btcLock from '../helpers/lockdrop/BitcoinLockdrop';
 import { BtcWalletType } from '../types/LockdropModels';
 import BtcRawSignature from '../components/BtcLock/BtcRawSignature';
 import TrezorLock from '../components/BtcLock/TrezorLock';
 import LedgerLock from '../components/BtcLock/LedgerLock';
-import TrezorConnect, { DEVICE } from 'trezor-connect';
+//import TrezorConnect, { DEVICE } from 'trezor-connect';
 import * as bitcoinjs from 'bitcoinjs-lib';
 import * as plasmUtils from '../helpers/plasmUtils';
 import { toast } from 'react-toastify';
@@ -59,7 +59,7 @@ export default function DustyBtcLockPage() {
     const classes = useStyles();
 
     const [walletType, setWalletType] = useState<BtcWalletType>(BtcWalletType.None);
-    const [startedTrezor, setTrezorInst] = useState(false);
+    //const [startedTrezor, setTrezorInst] = useState(false);
     const [plasmApi, setPlasmApi] = useState<ApiPromise>({} as ApiPromise);
     const [isLoading, setLoading] = useState<{
         loadState: boolean;
@@ -69,49 +69,49 @@ export default function DustyBtcLockPage() {
         message: '',
     });
 
-    const handleTrezor = () => {
-        // only initialize if there isn't a instance
-        if (startedTrezor) {
-            setWalletType(BtcWalletType.Trezor);
-        } else {
-            // create event listener to log device events
-            TrezorConnect.on('DEVICE_EVENT', event => {
-                if (event.type === DEVICE.CONNECT) {
-                    console.log('connected to Trezor device');
-                } else if (event.type === DEVICE.DISCONNECT) {
-                    console.log('disconnected to Trezor device');
-                }
-            });
-            // initialize trezor instance
-            TrezorConnect.init({
-                manifest: {
-                    email: 'developers@stake.co.jp',
-                    appUrl: 'https://lockdrop.plasmnet.io',
-                },
-                debug: true,
-                lazyLoad: true,
-                popup: true,
-            })
-                .then(() => {
-                    console.log('connected to Trezor');
-                    setWalletType(BtcWalletType.Trezor);
-                    setTrezorInst(true);
-                })
-                .catch(e => {
-                    console.log('something went wrong\n' + e);
-                });
-        }
-    };
+    // const handleTrezor = () => {
+    //     // only initialize if there isn't a instance
+    //     if (startedTrezor) {
+    //         setWalletType(BtcWalletType.Trezor);
+    //     } else {
+    //         // create event listener to log device events
+    //         TrezorConnect.on('DEVICE_EVENT', event => {
+    //             if (event.type === DEVICE.CONNECT) {
+    //                 console.log('connected to Trezor device');
+    //             } else if (event.type === DEVICE.DISCONNECT) {
+    //                 console.log('disconnected to Trezor device');
+    //             }
+    //         });
+    //         // initialize trezor instance
+    //         TrezorConnect.init({
+    //             manifest: {
+    //                 email: 'developers@stake.co.jp',
+    //                 appUrl: 'https://lockdrop.plasmnet.io',
+    //             },
+    //             debug: true,
+    //             lazyLoad: true,
+    //             popup: true,
+    //         })
+    //             .then(() => {
+    //                 console.log('connected to Trezor');
+    //                 setWalletType(BtcWalletType.Trezor);
+    //                 setTrezorInst(true);
+    //             })
+    //             .catch(e => {
+    //                 console.log('something went wrong\n' + e);
+    //             });
+    //     }
+    // };
 
     const isPanelOpen = (panelWallet: BtcWalletType) => {
         return walletType === panelWallet;
     };
 
-    const handleLedger = () => {
-        //todo: implement this
-        console.log('logging in to Ledger');
-        setWalletType(BtcWalletType.Ledger);
-    };
+    // const handleLedger = () => {
+    //     //todo: implement this
+    //     console.log('logging in to Ledger');
+    //     setWalletType(BtcWalletType.Ledger);
+    // };
 
     const handleRawTx = () => {
         setWalletType(BtcWalletType.Raw);
@@ -188,23 +188,23 @@ export default function DustyBtcLockPage() {
                             </IonCardHeader>
 
                             <IonCardContent>
-                                <IonItem
+                                {/* <IonItem
                                     button
                                     onClick={() => handleTrezor()}
                                     disabled={isPanelOpen(BtcWalletType.Trezor)}
                                 >
                                     <IonIcon icon={trezorLogo} slot="start" />
                                     <IonLabel>Sign in with Trezor</IonLabel>
-                                </IonItem>
+                                </IonItem> */}
 
-                                <IonItem
+                                {/* <IonItem
                                     button
                                     onClick={() => handleLedger()}
                                     disabled={isPanelOpen(BtcWalletType.Ledger)}
                                 >
                                     <IonIcon icon={ledgerLogo} slot="start" />
                                     <IonLabel>Sign in with Ledger</IonLabel>
-                                </IonItem>
+                                </IonItem> */}
 
                                 <IonItem button onClick={() => handleRawTx()} disabled={isPanelOpen(BtcWalletType.Raw)}>
                                     <IonIcon icon={warning} slot="start" />
