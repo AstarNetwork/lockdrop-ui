12a4fd2a851375ac65ece95bbed6bb7359862e15
Slobodan Kovacevic <slobodan.kovacevic@8sigma.eu>
2021-08-06 08:16:51 +0200

    @plasm/types removed

--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -2,18 +2,54 @@ import React, { useMemo, useState, useEffect, useContext } from 'react';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { ApiContext, ApiProps } from '../contexts/ApiContext';
 import { PlasmNetwork } from '../helpers/plasmUtils';
-import { plasmDefinitions, dustyDefinitions } from '@plasm/types';
 import { getNetworkEndpoint } from '../config/endpoints';
 
 const DEFAULT_NETWORK = PlasmNetwork.Local;
 let api: ApiPromise;
+
+// Ignore camel case complaints.
+/* eslint-disable */
+const types = {
+    AccountInfo: 'AccountInfoWithRefCount',
+    AuthorityId: 'AccountId',
+    AuthorityVote: 'u32',
+    Claim: {
+        amount: 'u128',
+        approve: 'BTreeSet<AuthorityId>',
+        complete: 'bool',
+        decline: 'BTreeSet<AuthorityId>',
+        params: 'Lockdrop',
+    },
+    ClaimId: 'H256',
+    ClaimVote: {
+        approve: 'bool',
+        authority: 'u16',
+        claim_id: 'ClaimId',
+    },
+    DollarRate: 'u128',
+    Keys: 'SessionKeys2',
+    Lockdrop: {
+        duration: 'u64',
+        public_key: '[u8; 33]',
+        transaction_hash: 'H256',
+        type: 'u8',
+        value: 'u128',
+    },
+    PredicateHash: 'H256',
+    RefCount: 'u8',
+    TickerRate: {
+        authority: 'u16',
+        btc: 'u128',
+        eth: 'u128',
+    },
+};
+/* eslint-enable *?
 
 /**
  * creates ApiPromise for a given network
  * @param network end point for the client to connect to
  */
 export function getApi(network: PlasmNetwork): ApiPromise {
-    const types = network === PlasmNetwork.Main ? plasmDefinitions : dustyDefinitions;
     const url = getNetworkEndpoint(network);
     const provider = new WsProvider(url);
     return new ApiPromise({
